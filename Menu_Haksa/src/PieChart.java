fa9d81c4916e5a08dd2feebb856556a2f3c3f18a
1class-011 <taegyu.dev@gmail.com>
2019-10-11 16:12:50 +0900

    chartcolorCommit;

--- Menu_Haksa/src/PieChart.java
+++ Menu_Haksa/src/PieChart.java
@@ -5,14 +5,7 @@ import java.sql.*;
 
 //학과별 도서 대출 현황을 pie chart로 보여주기
 public class PieChart extends JPanel {
-
-	//InputPanel inputPanel = new InputPanel();
 	ChartPanel chartPanel = new ChartPanel();
-	
-	static int firstValue = 0;
-	static int secondValue = 0;
-	static int thirdValue = 0;
-	static int fourthValue = 0;
 	
 	Connection conn = null;
 	Statement stmt = null;
@@ -24,8 +17,6 @@ public class PieChart extends JPanel {
 		
 		setLayout(new BorderLayout());
 		
-		//add(inputPanel, BorderLayout.NORTH);
-		
 		add(chartPanel, BorderLayout.CENTER);
 		
 
@@ -33,53 +24,6 @@ public class PieChart extends JPanel {
 		this.setVisible(true);
 		
 	}
-/*
-	class InputPanel extends JPanel
-	{
-		public InputPanel()
-		{
-			this.setBackground(Color.LIGHT_GRAY);
-	
-			JLabel firstL = new JLabel("1/4분기");
-			this.add(firstL);
-			JTextField firstT = new JTextField("0", 5);
-			this.add(firstT);
-			
-			JLabel secondL = new JLabel("2/4분기");
-			this.add(secondL);
-			JTextField secondT = new JTextField("0", 5);
-			this.add(secondT);
-			
-			JLabel thirdL = new JLabel("3/4분기");
-			this.add(thirdL);
-			JTextField thirdT = new JTextField("0", 5);
-			this.add(thirdT);
-			
-			JLabel fourthL = new JLabel("4/4분기");
-			this.add(fourthL);
-			JTextField fourthT = new JTextField("0", 5);
-			this.add(fourthT);
-			
-			JButton inputBtn = new JButton("Draw Pie Chart!");
-			this.add(inputBtn);
-			inputBtn.addActionListener(new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					
-					firstValue = Integer.parseInt(firstT.getText());
-					secondValue = Integer.parseInt(secondT.getText());
-					thirdValue = Integer.parseInt(thirdT.getText());
-					fourthValue = Integer.parseInt(fourthT.getText());
-								
-					chartPanel.repaint();
-
-				}});
-			
-		}
-
-	}
-*/
 	
 	class ChartPanel extends JPanel
 	{
@@ -104,7 +48,6 @@ public class PieChart extends JPanel {
 				dept = new String[rowcnt];
 				bookCount = new int[rowcnt];
 				
-				//System.out.println(rowcnt);
 				int i = 0;
 				while(rs.next())
 				{
@@ -126,16 +69,10 @@ public class PieChart extends JPanel {
 			{
 				e.printStackTrace();
 			}
-					
 
-			/*
-			for(int j = 0; j < rowCount; j++)
-			{
-				System.out.println(dept[j] + bookCount[j]);
-			}
-			*/	
-					
-					
+			//color array로 하고싶은데...
+			//Color[] chartColor = {new Color(100,20,30), new Color(10,200,30), new Color(10,20,300)};
+			
 			super.paintComponent(g);
 			try 
 			{
@@ -149,40 +86,12 @@ public class PieChart extends JPanel {
 				for (int i = 0; i < rowCount; i++)
 				{
 					g.drawString(dept[i], 380, 140 + 20*i);	//legend_name
-					g.setColor(new Color(i*30, 10, 10));
-					
-					g.fillArc(30, 20, 300, 300, angleSum, 360*bookCount[i]/sum);
-					angleSum += 360*bookCount[i]/sum;
+					g.setColor(new Color(i*60, 100, 100));
+					g.fillArc(30, 20, 300, 300, angleSum, 360*bookCount[i]/sum + 1);
+					angleSum += 360*bookCount[i]/sum + 1;	//int 값 angle로 인한 오차 보정(+1)
 					g.fillRect(360, 130+20*i, 12, 12);
-					System.out.println("start : " +angleSum+"angle :"+ 360*bookCount[i]/sum);
 				}
-				/*
-				int pFirst = 360*firstValue/sum;
-				int pSecond = 360*secondValue/sum;
-				int pThird = 360*thirdValue/sum;
-				int pFourth = 360*fourthValue/sum;
-				*/
 				
-				/*
-				g.drawString("1/4분기", 380, 140);
-				g.drawString("2/4분기", 380, 160);
-				g.drawString("3/4분기", 380, 180);
-				g.drawString("4/4분기", 380, 200);
-				*/
-				/*
-				g.setColor(new Color(192, 0, 0));
-				g.fillArc(30, 20, 300, 300, 0, pFirst+1);
-				g.fillRect(360, 130, 12, 12);
-				g.setColor(new Color(255, 192, 0));
-				g.fillArc(30, 20, 300, 300, pFirst+1, pSecond+1);
-				g.fillRect(360, 150, 12, 12);
-				g.setColor(new Color(19, 133, 53));
-				g.fillArc(30, 20, 300, 300, pFirst+pSecond+2, pThird+1);
-				g.fillRect(360, 170, 12, 12);
-				g.setColor(new Color(0, 112, 192));
-				g.fillArc(30, 20, 300, 300, pFirst+pSecond+pThird+3, pFourth);
-				g.fillRect(360, 190, 12, 12);
-				*/
 			}
 			catch(Exception e) {}
 			
