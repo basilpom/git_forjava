a019ea8684d7109adaac913754d078ea183e7ff0
1class-011 <taegyu.dev@gmail.com>
2019-10-22 18:39:07 +0900

    LoginDialogViewCommit;

--- Menu_Haksa/src/MenuHaksa.java
+++ Menu_Haksa/src/MenuHaksa.java
@@ -1,25 +1,111 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 
-
+class LoginDialog extends JDialog
+{
+	private JTextField tfID = new JTextField(10);
+	private JPasswordField pfPW = new JPasswordField(10);
+	private JButton loginBtn = new JButton("로그인");
+	private JButton cancelBtn = new JButton("취소");
+	
+	public LoginDialog(JFrame frame, String title)
+	{
+		super(frame, title, true);
+		this.setLayout(new FlowLayout());
+		ImageIcon loginImg = new ImageIcon("img/login.png");
+		JLabel imgLabel = new JLabel(loginImg);
+		JLabel titleLabel = new JLabel("관리자 로그인");
+		titleLabel.setFont(new Font("맑은 고딕", Font.BOLD, 30));
+		add(imgLabel);
+		add(titleLabel);
+		add(new JLabel("    아이디 : "));
+		add(tfID);
+		add(new JLabel("비밀번호 : "));
+		add(pfPW);
+		
+		
+		
+		loginBtn.addActionListener(new ActionListener() {
+			Connection conn = null;
+			Statement stmt = null;
+			ResultSet rs = null;
+			String password;
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//DB내의 로그인 정보와 일치하면 menuitem 활성화
+				DBManager db = new DBManager();
+				conn = db.getConnection();
+				
+				String query = "SELECT ADM_PW "
+							 + " FROM ADMINISTRATOR "
+							 + " WHERE ADM_ID = '"+tfID.getText()+"'";
+				
+				try 
+				{
+					stmt = conn.createStatement();
+					rs = stmt.executeQuery(query);
+					System.out.println(query);
+					while(rs.next())
+					{
+						password = rs.getString("ADM_PW");
+						System.out.println(password);
+					}
+					
+					if(password == String.valueOf(pfPW.getPassword()))
+					{
+						//로그인성공!
+					}
+					else
+					{
+						//ID, PW를 확인하세요!
+					}
+				}
+				catch(Exception e1)
+				{
+					e1.printStackTrace();
+				}
+			}});
+		cancelBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}});
+		
+		add(loginBtn);
+		add(cancelBtn);
+		
+		this.setBackground(Color.GRAY);
+		this.setSize(250, 320);
+	}
+}
 
 public class MenuHaksa extends JFrame{
 	JPanel panel = null;	//container 역할
 	Connection conn = null;
 	Student StudentObject = null;
 	JFrame frame = new JFrame();
+	private LoginDialog dialog;
 	public MenuHaksa()
 	{	
 		this.setTitle("학사관리");
@@ -110,11 +196,12 @@ public class MenuHaksa extends JFrame{
 		JButton btnStart = new JButton("시작하기");
 		panel.add(btnStart);
 		//Login Dialog 
+		dialog = new LoginDialog(this,"관리자로 로그인하기");
 		btnStart.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//Login 성공 시 menu item 활성화
-				
+				dialog.setVisible(true);
 				
 			}});
 		ImageIcon catIcon = new ImageIcon("img/newcat.png");
