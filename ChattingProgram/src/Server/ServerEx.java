30d9d4076cce6564030fa66e6670f7bbc6b345bf
taegyu <taegyu.dev@gmail.com>
2020-03-02 14:25:03 +0900

    Add Chatting Program

---
+++ ChattingProgram/src/Server/ServerEx.java
@@ -0,0 +1,61 @@
+package Server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ServerEx {
+
+	public static void main(String[] args)
+	{
+		BufferedReader in = null;
+		BufferedWriter out = null;
+		ServerSocket listener = null;
+		Socket socket = null;
+		Scanner scanner = new Scanner(System.in);
+		
+		try
+		{
+			listener = new ServerSocket(9999);
+			System.out.println("연결 기다리는 중...");
+			socket = listener.accept();
+			System.out.println("연결 되었습니다!");
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			while(true)
+			{
+				String inputMessage = in.readLine();
+				if(inputMessage.equalsIgnoreCase("bye"))
+				{
+					System.out.println("클라이언트에서 bye로 연결종료");
+					break;
+				}
+				System.out.println("클라이언트 : " + inputMessage);
+				System.out.print("보내기 >> ");
+				String outputMessage = scanner.nextLine();
+				out.write(outputMessage + "\n");
+				out.flush();
+			}
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				scanner.close();
+				socket.close();
+				listener.close();
+			}
+			catch(IOException e)
+			{
+				System.out.println("클라이언트와 채팅 중 오류가 발생했습니다.");
+			}
+			
+			
+		}
+		
+	}
+}
